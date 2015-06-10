01e9a29f20bdf503c0c4288f18eb15242cb530bd
LibinJ <libjia2@seas.upenn.edu>
2015-06-10 09:49:12 -0700

    Revert "Revert "page""
    This reverts commit ddc4d8c9a6483f76dde17529b7df57d56fe3b406.

---
+++ src/TextButton.js
@@ -0,0 +1,50 @@
+var Node = require('famous/core/Node');
+var DOMElement = require('famous/dom-renderables/DOMElement');
+var Scale = require('famous/components/Scale');
+var Size = require('famous/components/size');
+var Align = require('famous/components/Align');
+
+function TextButton(mount) {
+    // Extend Node
+    Node.call(this);
+
+    this
+        .setSizeMode('absolute', 'absolute')
+        .setAbsoluteSize(innerWidth*.2, innerWidth*.2)
+        .setAlign(0.5, 0.9)
+        .setMountPoint(0.5, 0.5)
+        .setOrigin(0.5, 0.5);
+
+    this.el = new DOMElement(this, { 
+        classes: ['TextButton'],
+        tagName: 'div',
+        content: 'T',
+        properties:{
+            fontSize: innerWidth * .08+'px',
+            textAlign: 'center',
+            fontFamily: 'Baskerville',
+            fontWeight: 'Bold',
+            color: '#FF9933',
+            zIndex: 32
+        }
+    });
+}
+
+// Extend the prototype
+TextButton.prototype = Object.create(Node.prototype);
+
+TextButton.prototype.animateFinal = function () {
+    var alignComp = new Align(this);
+    alignComp.set(0.2, 0.95, 0, {
+        duration: 600,
+        curve: 'outBounce'
+    })
+
+    var whiteSizeComponent = new Scale(this);
+    whiteSizeComponent.set(1, 1, 1, {
+        duration: 600,
+        curve: 'outBounce'
+    });
+};
+
+module.exports = TextButton;

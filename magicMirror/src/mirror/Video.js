f5ffd32b8d41a5f3b4b0365ba086a8d6c22d7cf3
Yahang Zhang <yahangz@seas.upenn.edu>
2015-06-11 11:58:39 -0700

    1st update

---
+++ magicMirror/src/mirror/Video.js
@@ -0,0 +1,23 @@
+var DOMElement = require('famous/dom-renderables/DOMElement');
+var Node = require('famous/core/Node');
+
+function Video(mount) {
+    // Extend Node
+    Node.call(this);
+    this.dom = new DOMElement(this, { 
+	    tagName: 'iframe',
+	    attributes: {
+	    	width: '100%',
+	    	height: '100%',
+	    	src: 'http://www.youtube.com/embed/TvKymDP85JI?rel=0&autoplay=1',
+	    	frameborder: 0
+	    },
+	    properties: {
+            zIndex: 10
+        }
+	});
+}
+
+Video.prototype = Object.create(Node.prototype);
+
+module.exports = Video;

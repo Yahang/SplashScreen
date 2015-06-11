2ec0d28374b59593202be67139dbf1127cffca05
LibinJ <libjia2@seas.upenn.edu>
2015-06-10 17:48:08 -0700

    rm

--- splashScreen/src/index.js
+++
@@ -1,9 +0,0 @@
-var SplashScreen = require('./SplashScreen');
-var FamousEngine = require('famous/core/FamousEngine');
-window.app = {
-   debug: true
-};
-
-FamousEngine.init();
-//create the app and pass in the target element
-var splashScreen = FamousEngine.createScene().addChild(new SplashScreen());
